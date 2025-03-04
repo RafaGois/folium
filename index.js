1faec6ab1ca4230dd953bacce410f5e6c089170e
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-03-04 15:48:17 -0300

    op

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ gsap.from(".name", {
   ease: "slow(0.7,0.7,false)",
 });
 
-gsap.from([".degree", ".title"], {
+gsap.from(".degree", {
   opacity: 0,
   duration:2.5,
   ease: "steps(12)",
   x: "400%"
 });
+
+gsap.from(".header .title", {
+    opacity: 0,
+    duration:2.5,
+    ease: "steps(12)",
+    y: "400%"
+  });
