0158914cb3852acdc53082d8c7c1d70e4ae00361
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-03-04 15:41:43 -0300

    up

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-document.querySelectorAll(".texto").forEach(elemento => {
-    elemento.style.color = "red";
+gsap.from(".name", {
+  opacity: 0,
+  y: -250,
+  duration: 2.5,
+  ease: "slow(0.7,0.7,false)",
+});
+
+gsap.from([".degree", ".title"], {
+  opacity: 0,
+  duration:2.5,
+  ease: "steps(12)",
+  x: "400%"
 });
