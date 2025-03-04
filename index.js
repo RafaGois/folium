45cb1abddc2c55b759dc0e935024c4914a3554d8
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-03-04 15:56:34 -0300

    up

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 gsap.from(".name", {
   opacity: 0,
   y: -250,
-  duration: 2.5,
+  duration: 1.5,
   ease: "slow(0.7,0.7,false)",
 });
 
 gsap.from(".degree", {
   opacity: 0,
-  duration:2.5,
+  duration: 7.5,
   ease: "steps(12)",
-  x: "400%"
+
 });
 
 gsap.from(".header .title", {
     opacity: 0,
-    duration:2.5,
-    ease: "steps(12)",
-    y: "400%"
+    y: 250,
+    duration: 1.5,
+    ease: "slow(0.7,0.7,false)",
+  });
+
+  gsap.from('.hero', {
+    duration: 1, // start the animation when ".box" enters the viewport (once)
+    x: 500
+});
+
+gsap.to(".about .box img", /* { x: 100 } */),
+  st = ScrollTrigger.create({
+    trigger: ".trigger",
+    start: "top center",
+    end: "+=500",
+    onUpdate: (self) => console.log("progress:", self.progress),
+    animation: tween,
   });
