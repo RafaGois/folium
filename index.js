ff7f7e08f1a78c237526bbfc3e703328e2449327
rafael.gois <api.mendes@mendesmaquinas.com.br>
2025-03-05 17:21:19 -0300

    up scroll effect

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
-gsap.from(".name", {
-  opacity: 0,
-  y: -250,
-  duration: 1.5,
-  ease: "slow(0.7,0.7,false)",
-});
+console.log("aa");
 
-gsap.from(".degree", {
-  opacity: 0,
-  duration: 7.5,
-  ease: "steps(12)",
-});
+document.addEventListener("DOMContentLoaded", function () {
 
-gsap.from(".header .title", {
+  gsap.from(".name", {
+    opacity: 0,
+    y: -250,
+    duration: 1.5,
+    ease: "slow(0.7,0.7,false)",
+  });
+
+  gsap.from(".degree", {
+    opacity: 0,
+    duration: 7.5,
+    ease: "steps(12)",
+  });
+
+  gsap.from(".header .title", {
     opacity: 0,
     y: 250,
     duration: 1.5,
     ease: "slow(0.7,0.7,false)",
   });
 
-  gsap.from('#hero', {
+  gsap.from("#hero", {
     duration: 2, // start the animation when ".box" enters the viewport (once)
-    x: "100%"
+    x: "100%",
+  });
+
+
+  const cards = [
+    {id: "#card-1", endTranslateX: -2000, rotate: 45},
+    {id: "#card-2", endTranslateX: -1000, rotate: -30},
+    {id: "#card-3", endTranslateX: -2000, rotate: 45},
+    {id: "#card-4", endTranslateX: -1500, rotate: -30},
+  ]
+
+  ScrollTrigger.create({
+    trigger: ".wrapper-404",
+    start: "top top",
+    end: "+=900vh",
+    scrub: 1,
+    pin: true,
+    onUpdate: (self) => {
+      gsap.to(".wrapper-404", {
+        x: `${-350 * self.progress}vw`,
+        duration: 0.5,
+        ease: "power3.out",
+      })
+    }
+  })
+
+  cards.forEach((card) => {
+    ScrollTrigger.create({
+      trigger: "card.id",
+      start: "top top",
+      start: "+=1200vh",
+      scrup: 1,
+      onUpdate: (self) => {
+        gsap.to(card.id, {
+          x: `${card.endTranslateX * self.progress}px`,
+          rotate: `${card.rotate * self.progress * 2}`,
+          duration: 0.5,
+          ease: "power3.out",
+        })
+      }
+    })
+  })
 });
